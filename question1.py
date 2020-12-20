6cfc34e9c2c5aa0e49269c08fb2dc1d6a984f045
Steven <tradefloltd@gmail.com>
2020-12-20 10:14:45 -0500

    q1 first attempt

--- question1.py
+++ question1.py
@@ -1,7 +1,17 @@
 def select_max(array):
     # write your function here
     # do NOT use the built-in max() function
-    pass
+    current_max = 0
+    if select_max() == False:
+        return None
+    else:
+        for i in select_max():
+            if i > current_max :
+                current_max = i
+        print(current_max)
+
+
+    #pass
 
 
 if __name__ == "__main__":
