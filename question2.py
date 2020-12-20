dfed3f55360e0e0c8a8ef8a9d2bca19673ccccc1
Steven <tradefloltd@gmail.com>
2020-12-20 16:00:51 -0500

    added for loop structure for q2. currently passes 1 test case. 4 test cases remain.

--- question2.py
+++ question2.py
@@ -5,7 +5,13 @@ def center_zeros(array):
     # write your function here
     # center means the floor(x / 2) where floor means rounding a float (decimal number) down to the nearest integer
     # i.e. floor(1) = 1, floor(1.5) = 1, floor(1.75) = 1, floor(2) = 2
-    pass
+    arr_len = len(array)
+    if array: # checking if array is empty
+        for i in array:
+            if (i == 0) and (array[:i] == floor(arr_len / 2)): # not sure if array[:i] returns an int or the value of the element
+                pass
+    else:
+        return []
 
 
 if __name__ == "__main__":
